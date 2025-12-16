39f4f94633a82c9995a9dcade42419ed4826509f
WisdomAvan <codedplanet54113@gmail.com>
2025-12-16 16:51:09 +0100

    Xmas Song

---
+++ cornflasks and nokia/Multiplier.py
@@ -0,0 +1,25 @@
+import java.util.scanner;
+public class StudentGrade{
+public static void main(String [] args){
+
+Scanner obtain = new Scanner(System.in);
+
+for(int count=1; counter <=15; counter++)
+
+    System.out.print("Enter Score: ");
+
+    double score = obtain.nextDouble();
+    
+    if(score >=45){
+
+}
+
+System.out.print("You passed the exam")
+
+
+
+
+
+
+    }
+}
---
+++ cornflasks and nokia/function_class.py
@@ -0,0 +1,21 @@
+def number_is_prime(number):
+    if number < 0:
+        number = number*-1
+
+
+
+
+#def number_is_prime(number):
+#    factors =0    
+ #   counter =2
+  #  while(counter < number):
+   #     if number%counter==0:
+    #        factors+=1
+     #   counter+=1
+   # if factors==0:
+    #    return True
+    #return False 
+
+
+
+
